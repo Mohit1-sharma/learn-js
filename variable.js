5aaae75d0787423ca5a63064f1dcf800ff509d72
Mohit1-sharma <114127144+Mohit1-sharma@users.noreply.github.com>
2025-02-01 08:15:38 +0000

    learned some datatypes.

---
+++ variable.js
@@ -0,0 +1,15 @@
+// in this module i ll learn some datatypes and i ll do investigate study on them
+let score = 'abs'
+//console.log(score)
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+let isLoggedIn = 1;
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanisLoggedIn);
+
+console.log(booleanisLoggedIn);
+
