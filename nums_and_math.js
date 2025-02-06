b783dc8524ca7d0936ce1bc1e7cff194050814e6
Mohit1-sharma <114127144+Mohit1-sharma@users.noreply.github.com>
2025-02-07 00:33:45 +0530

    learned math functions

--- nums_and_math.js
+++ nums_and_math.js
@@ -1,15 +1,30 @@
 const score = 400
-console.log(score)
+//console.log(score)
 
 const balance = new Number(100)
-console.log(balance)
+//console.log(balance)
 
-console.log(balance.toFixed(2))
+//console.log(balance.toFixed(2))
 
 const otherNumber = 23.8966
 
-console.log(otherNumber.toPrecision(2))
+//console.log(otherNumber.toPrecision(2))
 
 const hundreds = 1000000;
-console.log(hundreds.toLocaleString('en-IN'));
+//console.log(hundreds.toLocaleString('en-IN'));
 
+// +++++++++Maths+++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(8.6))
+// console.log(Math.ceil(8.2))
+// console.log(Math.floor(9.9))
+
+console.log(Math.random());
+// value will be froom 0 to 1
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1))+min)
