a23e22015339ff58f2f7724e4e5306545f583ce9
Mohit1-sharma <114127144+Mohit1-sharma@users.noreply.github.com>
2025-02-09 14:05:30 +0530

    commit message

--- nums_and_math.js
+++ nums_and_math.js
@@ -24,8 +24,11 @@ const hundreds = 1000000;
 console.log(Math.random());
 // value will be froom 0 to 1
 
-const min = 10
-const max = 20
 
-console.log(Math.floor(Math.random() * (max-min+1))+min)
-console.log("apsara beboo")
+
+console.log("Hello");
+
+const min = 1
+const max = 6
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
