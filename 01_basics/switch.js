09d0eecaf84f0ca784a6a9206dd8164fa471e39d
Mohit1-sharma <114127144+Mohit1-sharma@users.noreply.github.com>
2025-03-25 22:48:52 +0530

    learned for loop

--- 01_basics/switch.js
+++ 01_basics/switch.js
@@ -33,16 +33,24 @@ switch (month) {
 // false, 0, -0,BigInt, "", null ,undefined, NaN
 
 // Nullish coalescing operator (??): null undefined
-let val1;
- val1 = 10 ?? null
+// let val1;
+//  val1 = 10 ?? null
 
 
 
 
 
 
- console.log(val1);
+//  console.log(val1);
 
- const iceTeaPrice = 100;
+//  const iceTeaPrice = 100;
 
- iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
+//  iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
+for (let i = 0; i <= 10; i++) {
+    console.log(`Outer loop value ${i}`);
+    
+    for (let j = 0; j <= 10; j++) {
+        console.log(`Inner loop value ${j} and Outer loop value ${i}`);
+        
+    }
+}
