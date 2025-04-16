d6aa0f494cbb9968a92740eba1b370f093ec2afe
Mohit1-sharma <114127144+Mohit1-sharma@users.noreply.github.com>
2025-04-16 07:46:51 +0530

    solved challenges

--- 01_basics/js_modeOn/nine.js
+++ 01_basics/js_modeOn/nine.js
@@ -49,14 +49,15 @@ let order = processTeaOrder(makeTea)
 
 
 
-function createTeaMaker(){
+function createTeaMaker(name){
+    let score = 100
     return function(teaType){
-        return `Making ${teaType}`;
+        return `Making ${teaType} ${name} ${score}`;
     }
 }
 
-let teaMaker = createTeaMaker()
+let teaMaker = createTeaMaker("Mohit")
 let result = teaMaker("green tea");
 
-console.log(result)
+console.log(result);
 
